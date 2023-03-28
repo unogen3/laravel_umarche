87eed7c0f45b1d982dea9ed535752267710d8ff5
unogen3 <ueno@heian-co.jp>
2023-03-28 15:28:59 +0900

    intervention/image

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\Shop;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
+use InterventionImage;
 
 class ShopController extends Controller
 {
@@ -34,6 +35,8 @@ class ShopController extends Controller
 
     public function index()
     {
+        // phpinfo();
+
         //$ownerId = Auth::id();
         $shops = Shop::where('owner_id', Auth::id())->get();
 
@@ -52,7 +55,14 @@ class ShopController extends Controller
     {
         $imageFile = $request->image;
         if(!is_null($imageFile) && $imageFile->isValid() ){
-            Storage::putFile('public/shops', $imageFile);
+            // Storage::putFile('public/shops', $imageFile);
+            $fileName = uniqid(rand().'_');
+            $extension = $imageFile->extension();
+            $fileNameToStore = $fileName. '.' . $extension;
+            $resizedImage = InterventionImage::make($imageFile)->resize(1920, 1080)->encode();
+            // dd($imageFile, $resizedImage);
+
+            Storage::put('public/shops/'. $fileNameToStore, $resizedImage );
         }
 
     return redirect()->route('owner.shops.index');
