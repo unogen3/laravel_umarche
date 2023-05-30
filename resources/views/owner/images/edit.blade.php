a77d07df94cc6e74afcc88a6ba2ff3e1694244a9
unogen3 <ueno@heian-co.jp>
2023-05-30 15:46:42 +0900

    EditUpdate

--- resources/views/owner/images/edit.blade.php
+++ resources/views/owner/images/edit.blade.php
@@ -18,7 +18,7 @@
                       <div class="p-2 w-1/2 mx-auto">
                         <div class="relative">
                           <label for="title" class="leading-7 text-sm text-gray-600">画像タイトル</label>
-                          <input type="title" id="title" name="title" value="{{ $image->title }}" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                          <input type="text" id="title" name="title" value="{{ $image->title }}" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                         </div>
                       </div>
 
--- resources/views/owner/shops/edit.blade.php
+++ resources/views/owner/shops/edit.blade.php
@@ -10,8 +10,8 @@
           <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
               <div class="p-6 bg-white border-b border-gray-200">
                 <x-auth-validation-errors class="mb-4" :errors="$errors" />  
-                <form method="post" action="{{  route('owner.shops.update', ['shop'=> $shop->id])}}" enctype="multipart/form-data">
-                    @csrf 
+                <form method="post" action="{{ route('owner.shops.update', ['shop' => $shop->id ])}}" enctype="multipart/form-data">
+                    @csrf
                     <div class="-m-2">
                       <div class="p-2 w-1/2 mx-auto">
                         <div class="relative">
@@ -21,18 +21,18 @@
                       </div>
                       <div class="p-2 w-1/2 mx-auto">
                         <div class="relative">
-                          <label for="infomation" class="leading-7 text-sm text-gray-600">店舗情報 ※必須</label>
+                          <label for="information" class="leading-7 text-sm text-gray-600">店舗情報 ※必須</label>
                           <textarea id="information" name="information" rows="10" required class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">{{ $shop->information }}</textarea>
                         </div>
                       </div>
-                          <div class="p-2 w-1/2 mx-auto">
-                            <div class="relative">
+                      <div class="p-2 w-1/2 mx-auto">
+                        <div class="relative">
                           <div class="w-32">
                             <x-thumbnail :filename="$shop->filename" type="shops" />
                           </div>
                         </div>
                       </div>
-                      <div class="p-2 w-1/2 mx-auto">
+                  <div class="p-2 w-1/2 mx-auto">
                         <div class="relative">
                           <label for="image" class="leading-7 text-sm text-gray-600">画像</label>
                           <input type="file" id="image" name="image" accept="image/png,image/jpeg,image/jpg" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
@@ -44,9 +44,9 @@
                           <div><input type="radio" name="is_selling" value="0" class="mr-2" @if($shop->is_selling === 0){ checked } @endif >停止中</div>
                         </div>
                       </div>
-                      <div class="p-2 w-full flex justify-around mt-4">
+                          <div class="p-2 w-full flex justify-around mt-4">
                         <button type="button" onclick="location.href='{{ route('owner.shops.index')}}'" class="bg-gray-200 border-0 py-2 px-8 focus:outline-none hover:bg-gray-400 rounded text-lg">戻る</button>
-                        <button type="submit" class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">更新する</button>
+                        <button type="submit" class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">更新する</button>                        
                       </div>
                     </div>
                   </form>
